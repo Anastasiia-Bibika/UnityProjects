74f5d2ad7908283d2ab55fcfa89754460156212f
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2024-04-10 20:12:45 +0300

    ForLaboratory4

---
+++ My project (1)/Assets/Scriots/GameManagerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+
+public class GameManagerScript : MonoBehaviour
+{
+    void Start()
+    {
+        GlobalStorage globalStorage = GlobalStorage.Instance;
+        int record = globalStorage.highScore;
+        int healthCount = globalStorage.health;
+        int collisionCount = globalStorage.collisions;
+        int collectedCoins = globalStorage.collectedCoins;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnApplicationQuit()
+    {
+        GlobalStorage globalStorage = GlobalStorage.Instance;
+        globalStorage.SaveData();
+    }
+
+}
